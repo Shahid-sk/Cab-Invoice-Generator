c6315728e1ca0da39250a9b918432912dfc5dde5
Shahid-sk <mail2sk.shahid@gmail.com>
2021-09-18 21:04:51 +0530

    added: created a class for enhanced invoice

--- src/com/bridgelabz/InvoiceGenerator.java
+++ src/com/bridgelabz/InvoiceGenerator.java
@@ -23,4 +23,26 @@ public class InvoiceGenerator {
 
 		return Math.max(totalFare, MINIMUM_FARE);
 	}
+
+	/**
+	 * Purpose : Given distance and time for multiple rides, Return aggregate total
+	 * fare for all the journey
+	 *
+	 * @param rides
+	 */
+	public double calculateTotalFare(Ride[] rides) {
+		double totalFare = 0.0;
+		for (Ride ride : rides) {
+			totalFare += this.calculateFare(ride.distance, ride.time);
+		}
+		return totalFare;
+	}
+
+	public InvoiceSummary calculateFareSummary(Ride[] rides) {
+		double totalFare = 0.0;
+		for (Ride ride : rides) {
+			totalFare += this.calculateFare(ride.distance, ride.time);
+		}
+		return new InvoiceSummary(rides.length, totalFare);
+	}
 }
--- src/com/bridgelabz/InvoiceServiceTest.java
+++ src/com/bridgelabz/InvoiceServiceTest.java
@@ -4,6 +4,10 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class InvoiceServiceTest {
 	InvoiceGenerator invoiceGenerator = null;
 
@@ -36,5 +40,28 @@ public class InvoiceServiceTest {
 		int time = 1;
 		double fare = invoiceGenerator.calculateFare(distance, time);
 		Assert.assertEquals(5, fare, 0.0);
+	}
+
+	/**
+	 * Step 2 Purpose : Given distance and time for multiple rides, return the
+	 * aggregate total for all the rides
+	 */
+	@Test
+	public void givenMultipleRides_ShouldReturnTotalFare() {
+		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+		double fare = invoiceGenerator.calculateTotalFare(rides);
+		Assert.assertEquals(30, fare, 0.0);
+	}
+
+	/**
+	 * Step 3 Purpose : Given distance and time, calculate the aggregate total for
+	 * all the rides finally calculate the average fare per ride.
+	 */
+	@Test
+	public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+		InvoiceSummary summary = invoiceGenerator.calculateFareSummary(rides);
+		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30);
+		Assert.assertEquals(expectedInvoiceSummary, summary);
 	}
 }
